1b6c5cce041c45cf3c967431afad50dff2426b46
pepe <oscar.novillo@gmail.com>
2022-10-16 18:42:34 +0200

    asd

--- implantacion/miprimer python/main.py
+++ implantacion/miprimer python/main.py
@@ -1,6 +1,10 @@
-
+# crear variables
 i = 8
 
+def jj(i):
+    return i+2
+
+# ejecucion cosas
 
 if ( __name__ == "__main__" ):
-    print("hola mundo ",i,"jlkj")
+    print("hola mundo ",jj(i),"jlkj")

deb2d3cddc34d4b0fc726d7ad1647658db3e43a8
pepe <oscar.novillo@gmail.com>
2022-10-16 18:42:07 +0200

    asdasd

---
+++ implantacion/miprimer python/variables.py
@@ -0,0 +1,19 @@
+a = "ll"
+b= 9
+c= 9.8
+
+
+nombre = input("ingrese su nombre: ")
+
+
+mensaje = "hola mundo "+nombre
+print("hola mundo ",nombre)
+print (mensaje)
+
+numero = input("ingrese un numero: ")
+
+if a == str():
+    n = 90
+    print("es un string")
+
+# print(n)

eefd65ac3255bcc48111b76932d4094fdade7749
oscarnovillo <oscar.novillo@gmail.com>
2022-10-18 11:13:22 +0200

    saas

---
+++ implantacion/miprimer python/peticionDatos.py
@@ -0,0 +1,8 @@
+nombre = input("Quien coño eres?\n")
+
+print("hola ", nombre)
+
+telefono = input(nombre+" cual es tu telefono?")
+
+datos = "nombre:"+nombre + "\ntelefono: "+ telefono
+print(datos)

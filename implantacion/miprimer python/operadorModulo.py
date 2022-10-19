816ad4ff02f99f46c0060556279a5a930cc94e60
oscarnovillo <oscar.novillo@gmail.com>
2022-10-19 10:07:03 +0200

    asdasd

---
+++ implantacion/miprimer python/operadorModulo.py
@@ -0,0 +1,7 @@
+num = 10
+num2 = 32
+
+
+modulo = num2 % num
+
+porcentaje = (modulo * 100) / 26
---
+++ implantacion/miprimer python/suma.py
@@ -0,0 +1,8 @@
+numero1 = int(input("ingrese un numero: "))
+numero2 = int(input("ingrese otro numero: "))
+
+suma = numero1 + numero2
+
+mensajeResultado = "la suma de " + str(numero1) +  " y " + str(numero2)+ " es "+ str(suma)
+
+print("la suma de ",numero1," y ",numero2, " es "+ str(suma))

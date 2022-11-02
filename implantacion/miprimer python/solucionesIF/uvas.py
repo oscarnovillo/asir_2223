466ededc8fc7bc46dce0bc507c93c291192930ec
oscarnovillo <oscar.novillo@gmail.com>
2022-11-02 11:03:47 +0100

    asdasd

---
+++ implantacion/miprimer python/solucionesIF/uvas.py
@@ -0,0 +1,21 @@
+# pedir datos
+precioKiloIncial=0
+
+tipoUva=input("¿Que tipo de uva es? (A o B): ")
+tamañoUva=0
+kilosUvas = 0
+
+if tipoUva == "a":
+    if tamañoUva == "1":
+        precioKiloIncial = 3
+    elif tamañoUva == "2":
+        precioKiloFinal = 90
+elif tipoUva == "b":
+    print("b")
+
+
+año = 2020
+
+
+
+print(precioKiloFinal)

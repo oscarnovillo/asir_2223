f779f04bc4ac835322f38c168b4098e6cef9578c
oscarnovillo <oscar.novillo@gmail.com>
2022-11-16 10:45:27 +0100

    asdasd

--- implantacion/miprimer python/solucionesBucles/sjupermercado.py
+++ implantacion/miprimer python/solucionesBucles/sjupermercado.py
@@ -1,46 +1,51 @@
 #pedir dia semana
 dia = input("¿Que dia es hoy? ")
-
+precioTotal = 0
 mensaje = "Hoy es domingo, no hay que trabajar"
+descuentoMartes = 0.2
 
 if (dia != "domingo"):
     #quien eres
     nombre = input("¿Como te llamas? ")
-    #pedir nombre producto
-    nombreProducto = input("¿Que producto quieres comprar? ")
-    #pedir cantidad producto
-    cantidadProducto = int(input("¿Cuantos productos quieres comprar? "))
-    #pedir nombre producto1
-    nombreProducto1 = float(input("¿Cual es el precio del producto 2? "))
-    #pedir cantidad producto2
-    cantidadProducto2 = int(input("¿Cuantos productos quieres comprar 2? "))
-    descuento = 1
-    #calcular precio producto
-    if (nombreProducto == "leche"):
-        precioProducto = 1
-    elif (nombreProducto == "pan"):
-        precioProducto = 0.5
-
-    if (dia == "lunes"):
+    for i in range(5):
+        #pedir nombre producto
+        nombreProducto = input("¿Que producto quieres comprar? ")
+        #pedir cantidad producto
+        cantidadProducto = int(input("¿que cantidad de "+str(nombreProducto)+" quieres comprar? "))
+        #pedir nombre producto1
+        descuento = 1
+        #calcular precio producto
         if (nombreProducto == "leche"):
-            descuento = 0.8
-    elif (dia == "martes"):
-        descuento = 1.2
-    elif (dia == "miercoles"):
-        if (nombreProducto == "pan"):
-            cantidadProducto = cantidadProducto // 2 + cantidadProducto % 2
-    elif (dia == "viernes"):
-        descuento = 0.3
+            precioProducto = 1
+        elif (nombreProducto == "pan"):
+            precioProducto = 0.5
 
-    #ver cliente
-    if (nombre == "carlos"):
-        if (nombreProducto=="leche") or (nombreProducto=="pan"):
-            descuento -= 0.1
-    elif (nombre == "mrMuro"):
-        descuento = 1.5
+        if (dia == "lunes"):
+            if (nombreProducto == "leche"):
+                descuento = 0.8
+        elif (dia == "martes"):
+            descuento = 1+descuentoMartes
+        elif (dia == "miercoles"):
+            if (nombreProducto == "pan"):
+                cantidadProducto = cantidadProducto // 2 + cantidadProducto % 2
+        elif (dia == "viernes"):
+            descuento = 0.3
 
-    #calcular precio total
-    precioTotal = (nombreProducto*cantidadProducto*descuento) 
+        #ver cliente
+        if (nombre == "carlos"):
+            if (nombreProducto=="leche") or (nombreProducto=="pan"):
+                descuento -= 0.1
+        elif (nombre == "mrMuro"):
+            descuento = 1.5
+            if (dia=="martes"):
+                descuento += descuentoMartes
 
+        #calcular precio total
+        precioProducto = (cantidadProducto*descuento) 
+        precioTotal += precioProducto
+        print("El precio de este producto es {:.02f}".format(precioProducto))
+        print(f"el dinero gastado que llevas es {precioTotal:.2f}")
 
-print(mensaje)
+    print(f"el precio total es {precioTotal:.2f}")
+else:
+    print(mensaje)

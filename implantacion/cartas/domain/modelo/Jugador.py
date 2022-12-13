4934efaedf3e3e52ac56c62e15270834ceed7036
pepe <oscar.novillo@gmail.com>
2022-12-13 21:27:00 +0100

    z

--- implantacion/cartas/domain/modelo/Jugador.py
+++ implantacion/cartas/domain/modelo/Jugador.py
@@ -1,11 +1,11 @@
 class Jugador:
 
-    def __init__(self, nombre,apellidos,puntos=0):
+    def __init__(self, nombre :str,apellidos:str ,puntos:int =0):
         self.nombre = nombre
         self.apellidos = apellidos
         self.puntos = puntos
 
-    def nombreCompleto(self):  
+    def nombre_completo(self):  
         return self.nombre + " " + self.apellidos
 
     def __str__(self):
--- implantacion/cartas/domain/modelo/baraja.py
+++ implantacion/cartas/domain/modelo/baraja.py
@@ -3,5 +3,16 @@ from domain.modelo.carta import Carta
 
 class Baraja:
     
+    indice : int = 0
     def __init__ (self):
         self.cartas : list[Carta] = list()
+        
+    def siguiente_carta(self):
+        if self.indice >= self.cartas.__len__():
+            return None
+        carta = self.cartas[self.indice]
+        self.indice += 1
+        return carta
+    
+    def dar_vuelta_baraja(self):
+        self.indice = 0

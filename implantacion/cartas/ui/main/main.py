8a5e1cbf22147f1e752a4f1d370111c3b3824208
oscarnovillo <oscar.novillo@gmail.com>
2022-12-14 09:36:01 +0100

    as

--- implantacion/cartas/ui/main/main.py
+++ implantacion/cartas/ui/main/main.py
@@ -1,23 +1,37 @@
-import sys
-
+from domain.modelo.jugador import Jugador
 from domain.modelo.baraja import Baraja
-from domain.servicios.servicios_cartas import crear_baraja, mezclar_baraja
+from domain.servicios.servicios_cartas import ServiciosCartas
 
 
 # ejecucion cosas
 def main():
-    print(__name__)
-    baraja : Baraja = crear_baraja()
-    # mezclar_baraja(baraja.cartas)
+    serviciosCartas = ServiciosCartas()
+    baraja : Baraja = serviciosCartas.crear_baraja()
+
+    serviciosCartas.mezclar_baraja(baraja.cartas)
     
-    #Pedir jugadores
+    #Pedir 2 jugadores
+
+    # Combat cartas
+
+
+    # sacar una carta para cada jugador
+
+    #un punto al jugador que la tenga mas grande
+
+    # al final cuando se acaba la baraja se dice quien gana
+
+
     
     carta = baraja.siguiente_carta()
     while (carta != None):
         print(carta)
         carta = baraja.siguiente_carta()
     
+        
     
     
-    # for carta in baraja.cartas:
-    #     print(carta)
+    
+    for carta in baraja.cartas:
+        print(carta)
+

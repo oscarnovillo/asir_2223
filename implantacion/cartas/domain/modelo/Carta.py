98fe84c18d30489a0ab64a66e79a4c910f2b32aa
pepe <oscar.novillo@gmail.com>
2022-12-16 21:07:23 +0100

    asdasd

--- implantacion/cartas/domain/modelo/Carta.py
+++ implantacion/cartas/domain/modelo/Carta.py
@@ -1,13 +1,17 @@
 from enum import Enum
+from dataclasses import dataclass,field
 
 
+@dataclass(order=True)
 class Carta:
+    
+    
     class PALOS(Enum):
         OROS = 1
         ESPADAS = 2
         COPAS = 3
         BASTOS = 4
-        
+
     class NUMEROS(Enum):
         AS = 1
         DOS = 2
@@ -18,11 +22,12 @@ class Carta:
         SIETE = 7
         SOTA = 10
         CABALLO = 11
-        REY = 12    
+        REY = 12
 
-    def __init__(self, valor : NUMEROS, palo:PALOS):
-        self.valor = valor
-        self.palo = palo
-        
+    sorted_by = "valor.value"
+    valor :NUMEROS
+    palo : PALOS
+
+
     def __str__(self) :
-        return "Cartita: " + str(self.valor.name) + " de " + str(self.palo.name)
+        return "Cartita: " + str(self.valor.value) + " de " + str(self.palo.name)
